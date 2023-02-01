b64ac5f64407396c71f4bcbe288dcf4239b2e561
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-02-01 18:57:55 +0100

    makes second part of homework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+// 1.
+
+const makeMarkup = (array) => {
+  return array
+    .map(
+      (element) =>
+        `<a class="gallery__item" href="${element.original}">
+         <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+    </a>`
+    )
+    .join("");
+};
+
+refs.gallery.insertAdjacentHTML("beforeend", makeMarkup(galleryItems));
+
+const onImageClick = (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let gallery = new SimpleLightbox(".gallery a");
+  gallery.on("show.simplelightbox", function () {});
+
+  gallery.on("error.simplelightbox", function (e) {
+    console.log(e);
+  });
+
+  gallery.options.captionsData = "alt";
+  gallery.options.captionDelay = 250;
+};
+
+refs.gallery.addEventListener("click", onImageClick);
